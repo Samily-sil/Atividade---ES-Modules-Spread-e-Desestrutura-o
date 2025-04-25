b43f9accde65b1764d15fa40b76b85912330669e
Samily <samily.sss2005@gmail.com>
2025-04-25 18:59:40 -0300

    finalizada

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,43 +1,51 @@
 import { usuarios } from "./dbUsers.js";
 import { enderecos } from "./dbAddress.js";
 
-/*
-  Importe corretamente as constantes usuarios e enderecos dos arquivos
-  dbUsers.js e dbAdress.js, respecitvamente
-*/
-
-// Faça as importações corretas acima
 console.log(usuarios);
 console.log(enderecos);
 
-// Adicionar endereços aos usuarios pelo id
 function addAddresToUser(listaDeUsuarios, listaDeEnderecos) {
-  return;
+  return listaDeUsuarios.map(usuario => {
+    const enderecoCorrespondente = listaDeEnderecos.find(endereco => endereco.userId === usuario.id);
+    
+    return {
+      ...usuario,
+      endereco: enderecoCorrespondente || {}
+    };
+  });
 }
 
-// Descomente os consoles abaixo para testar suas funções
-// console.log(addAddresToUser(usuarios, enderecos));
+console.log(addAddresToUser(usuarios, enderecos));
 
-// Funcao para deletar o password
-function deletePassword() {
-  return;
+// function deletePassword(usuario) {
+//   const { password, ...usuarioSemPassword } = usuario;
+//   return usuarioSemPassword;
+// }
+function deletePassword({ password, ...rest }) {
+  return rest;
 }
-// console.log(deletePassword(usuarios[0]));
+console.log(deletePassword(usuarios[0]));
 
 // Listar os usuarios sem a senha
 function listUsers(usuarios) {
-  return;
+  return usuarios.map((user) => deletePassword(user));
 }
-// console.log(listUsers(usuarios));
+console.log(listUsers(usuarios));
 
 // Listar somente nome, email e stack
 function listUserStacks(listaDeUsuarios) {
-  return;
+  return listaDeUsuarios.map (({nome, email, stacks}) => ({
+    nome,
+    email,
+    stacks,
+  }));
 }
-// console.log(listUserStacks(usuarios));
+console.log(listUserStacks(usuarios));
 
 //Deletar um usuário que tenha python na stack
 function deleteUser(listaDeUsuarios) {
-  return;
+  return listaDeUsuarios
+  .filter(({ stacks}) => !stacks.includes("Python"))
+  .map((user) => deletePassword(user));
 }
-// console.log(deleteUser(usuarios));
+console.log(deleteUser(usuarios));
